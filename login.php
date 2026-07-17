e17b08e4d6ee0fb653bcc350cf6a09de1eda16de
Wareesha <Wareeshatanweer.si@gmail.com>
2026-07-18 01:07:33 +0800

    fix dockerfile and paths

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 // 关闭报错显示，防止 JSON 格式被破坏
-ini_set('display_errors', 0);
-error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 header("Content-Type: application/json; charset=UTF-8");
 require_once 'db.php'; 
